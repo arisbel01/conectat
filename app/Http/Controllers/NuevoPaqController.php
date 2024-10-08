56f3600145e441184abbd3ecba9f1cd9bf13aeea
Rodrigodzl1 <163228467+Rodrigodzl1@users.noreply.github.com>
2024-10-08 03:37:58 -0600

    Agregar

---
+++ app/Http/Controllers/NuevoPaqController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NombresPaquetes;
+use Illuminate\Http\Request;
+
+class NuevoPaqController extends Controller
+{
+
+    public function index(): mixed {
+        
+    }
+}
+
