c2dd5883ed0a04172bc0768d2108f05319bf7374
Rodrigodzl1 <163228467+Rodrigodzl1@users.noreply.github.com>
2024-10-08 00:01:31 -0600

    CREAR PAQUETES

--- resources/views/agregar-paquete.blade.php
+++ resources/views/agregar-paquete.blade.php
@@ -3,6 +3,7 @@
 
 <head>
 
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,6 +11,10 @@
     <meta name="author" content="">
 
     <title>SB Admin 2 - Dashboard</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js"></script>
 
     
 </head>
