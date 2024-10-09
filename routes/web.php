671a856c83ce446fa1f0257941bdf7b75c114ef9
MrCris-SC <cristino_1401@hotmail.com>
2024-10-08 21:46:27 -0600

    new commit for add packs

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,11 @@ use App\Http\Controllers\PaqueteController;
 
 Route::get('/agregar-paquete', [PaqueteController::class, 'create']);
 Route::post('/agregar-paquete', [PaqueteController::class, 'store']);
+Route::get('/editar-paquete/{id}', [PaqueteController::class, 'edit'])->name('paquete.edit');
+Route::put('/editar-paquete/{id}', [PaqueteController::class, 'update'])->name('paquete.update');
 
 
-Route::get('/', [PaquetesController::class, 'index']);
+Route::get('/', [PaquetesController::class, 'index'])->name('paquete.index');
 
 Route::get('/recuperar', function () {return view('password');});
 
