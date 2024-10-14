eec3ce0135a17670b3a704bba4c80d4e57eee7d4
MrCris-SC <cristino_1401@hotmail.com>
2024-10-13 21:00:51 -0600

    user index

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaquetesController;
-
-
-
+use App\Http\Controllers\userController;
 use App\Http\Controllers\PaqueteController;
 
 Route::get('/agregar-paquete', [PaqueteController::class, 'create']);
@@ -15,9 +13,10 @@ Route::delete('/paquete/{id}', [PaqueteController::class, 'destroy'])->name('paq
 
 Route::get('/', [PaquetesController::class, 'index'])->name('paquete.index');
 
+Route::get('/user', [userController::class, 'showPackages'])->name('mostrar.paquetes');
+
 Route::get('/recuperar', function () {return view('password');});
 
 Route::get('/registro', function () {return view('registro');});
 Route::get('/login', function () {return view('login');});
 Route::get('/datos', function () {return view('datos');});
-Route::get('/user', function () {return view('user');});
