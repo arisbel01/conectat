e07ffe9a030af6ae6b93049b3ae0bba1cdf0f68c
MrCris-SC <cristino_1401@hotmail.com>
2024-10-18 19:44:22 -0600

    navvar pdate

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -29,18 +29,12 @@
 
                 <img src="{{asset('images/logo1.png')}}" alt="Logo" class="logo-img">
             </div>
-            <nav>
-                <ul>
-                    <li><a href="user">Inicio</a></li>
-                    <li><a href="user">Planes de Internet</a></li>
-
-
-                <img src="{{ asset('images/logonuevo.png') }}" alt="Logo" class="logo-img">
-            </div>
+           
             <nav>
                 <ul class="nav-links">
-                    <li><a href="#inicio">Inicio</a></li>
-                    <li><a href="#planes">Planes de Internet</a></li>
+                <li><a href="user">Inicio</a></li>
+                    <li><a href="user">Planes de Internet</a></li>
+
 
                     <li><a href="#contacto">Contacto</a></li>
                     <li><a href="#soporte">Soporte</a></li>
